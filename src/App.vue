ddd98fccc3fd1e72da6420c6cfbaa533bcf81439
Fatemeh <fatemehfaryadras7@gmail.com>
2023-07-03 12:20:13 +0330

    add project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <TodoList></TodoList>
+</template>
+
+<script>
+
+import './assets/tailwind.css';
+import TodoList from './components/TodoList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="flex items-center justify-center">
+    <input v-model="list" class="rounded border mx-2 px-3 py-2" type="text">
+  <button @click="add" class="bg-blue-400 px-5 py-2 rounded text-white">ADD</button>
+  </div>
+  <ul>
+    <li class="bg-blue-300 w-[300px] px-3 py-2 mt-5 rounded mx-auto flex" v-for="(list , index) in lists" :key="index">
+        {{list.text}}
+       <div class="flex mx-auto mr-1">
+        <button class="mx-1" @click="remove()">❌</button>
+        <button class="mx-1" @click="edit(index)">🖍️</button>
+       </div>
+    </li>
+    
+  </ul>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            editTask:null,
+              list:'',
+              lists : [
+              
+              ],
+        };
+    },
+    methods : {
+        add(){
+            if(this.lists.length === '') return ;
+            
+            if(this.editTask === null){
+                this.lists.push({
+                    id : Math.random(),
+                    text :this.list
+                })
+            }else{
+                this.lists[this.editTask].text=this.list;
+                 this.editTask=null;
+            }
+            this.list=''
+        },
+        remove(index){
+            this.lists.splice(index, 1)
+        },
+        edit(index){
+            this.list=this.lists[index].text;
+            this.editTask=index;
+        }
+    }
+}
+</script>
